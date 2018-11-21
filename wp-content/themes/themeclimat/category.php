901bbd6c0013faad939da72dc6199a10b6e6fe7e
vicgngd <victor.girongoudard@gmail.com>
2018-11-21 22:14:20 +0100

    Link articles pages

--- wp-content/themes/themeclimat/category.php
+++ wp-content/themes/themeclimat/category.php
@@ -15,10 +15,12 @@
               $the_query-> the_post();
       ?>
       <div>
-          <?php the_post_thumbnail( 'article-preview_thumb', array(
-            'class' => "article-thumbnail",'alt' => get_the_title() ) ); ?>
-          <h4><?php the_title() ?></h4>
-          <p><?php the_field('accroche') ?></p>
+          <a href="<?php the_permalink() ?>">
+            <?php the_post_thumbnail( 'article-preview_thumb', array(
+              'class' => "article-thumbnail",'alt' => get_the_title() ) ); ?>
+            <h4><?php the_title() ?></h4>
+            <p><?php the_field('accroche') ?></p>
+          </a>
       </div>
       <?php
           }
