f5be015d28a3db0bf35e9758bf62d41afa9b9552
vicgngd <victor.girongoudard@gmail.com>
2018-11-22 09:59:31 +0100

    Display post content on single.php

--- wp-content/themes/themeclimat/single.php
+++ wp-content/themes/themeclimat/single.php
@@ -1,1 +1,12 @@
-<?php echo (the_title())?>
+<?php get_header() ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+ <?php the_title(); ?>
+ <?php the_post_thumbnail();?>
+ <?php the_content(); ?>
+
+<?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer() ?>
