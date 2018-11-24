1c6e5bc126ff3f383f847868d4be1edf32732829
vicgngd <victor.girongoudard@gmail.com>
2018-11-24 12:28:19 +0100

    Add ongs on single pages

--- wp-content/themes/themeclimat/single.php
+++ wp-content/themes/themeclimat/single.php
@@ -9,4 +9,16 @@
 <?php endwhile; ?>
 <?php endif; ?>
 
+ <?php
+  $ongs = get_field('ongs');
+  foreach($ongs as $ong){
+      $ong_name = $ong->post_title;
+      $ong_link = get_field('lien_don', $ong->ID);
+      $ong_thumb = get_the_post_thumbnail( $ong->ID, 'ong_thumb' );
+      echo $ong_name;
+      echo $ong_thumb;
+      echo $ong_link;
+  }
+?>
+
 <?php get_footer() ?>
