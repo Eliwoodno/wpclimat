6b9d79f95f52d46f6dd9266e76b7020a5978c36a
Eliwoodno <pbarraux@gmail.com>
2018-11-27 23:06:54 +0100

    sliding anchor link

--- wp-content/themes/themeclimat/assets/scripts/main.js
+++ wp-content/themes/themeclimat/assets/scripts/main.js
@@ -18,7 +18,7 @@ $(window).on('resize', () => {
 
 function scrollToAnchor(name){
     let aTag = $("div[name='"+ name +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    $('html,body').animate({scrollTop: aTag.offset.top},'slow');
 }
 
 $(".link-to-ongs").click(function() {
