ad654d3ed65e16082f03b578f7e5a9ad9f599335
Eliwoodno <pbarraux@gmail.com>
2018-11-28 03:30:50 +0100

    front-end modifications

--- wp-content/themes/themeclimat/inc/support-thumbnails.php
+++ wp-content/themes/themeclimat/inc/support-thumbnails.php
@@ -3,7 +3,7 @@ function thumbnails_theme_support(){
     add_theme_support( 'post-thumbnails' );
 
     add_image_size( 'ong_thumb', 120, 120, false );
-    add_image_size( 'article-preview_thumb', 300, 250, false );
+    add_image_size( 'article-preview_thumb', 300, 250, true );
     add_image_size( 'etude-preview_thumb', 500, 400, true );
 }
 add_action( 'after_setup_theme', 'thumbnails_theme_support' );
